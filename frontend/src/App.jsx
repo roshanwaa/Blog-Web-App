bc5c28a643406cf8520d1cba721ae8452ae7d845
Mr.Smoke <kumroshan120@gmail.com>
2023-03-26 13:24:41 +0530

    Blog Page Starting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { FaCircle } from 'react-icons/fa';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <FaCircle />
+        <a href="">My Blog App</a>
+      </header>
+    </main>
+  );
+}
+
+export default App;
